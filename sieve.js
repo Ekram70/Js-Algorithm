911f2fdd35f60e3201507a5af41c22b6000ed7ce
Ekram70 <ekramullah70@gmail.com>
2022-04-22 23:28:41 +0600

    sieve algorithm to find 0 to nth prime number

--- sieve.js
+++ sieve.js
@@ -1,4 +1,4 @@
-var countPrimes = function (n) {
+function primes(n) {
   let arr = new Array(n).fill(true);
   for (let i = 2; i * i <= n; i++) {
     if (arr[i] == true) {
@@ -8,10 +8,10 @@ var countPrimes = function (n) {
     }
   }
 
-  let count = 0;
+  let primes = [];
   for (let i = 2; i < arr.length; i++) {
-    if (arr[i] == true) count++;
+    if (arr[i] == true) primes.push(i);
   }
 
-  return count;
-};
+  return primes;
+}
